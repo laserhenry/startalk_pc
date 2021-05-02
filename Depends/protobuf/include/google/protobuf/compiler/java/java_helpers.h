01f47d5d7e310363d9d50255bb7c67c4927048c6
laser <laserhenry@gmail.com>
2021-05-02 13:27:02 +0100

    Update java_helpers.h

--- Depends/protobuf/include/google/protobuf/compiler/java/java_helpers.h
+++ Depends/protobuf/include/google/protobuf/compiler/java/java_helpers.h
@@ -31,6 +31,7 @@
 // Author: kenton@google.com (Kenton Varda)
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
+//  Startalk Ltd. 2021 revised
 
 #ifndef GOOGLE_PROTOBUF_COMPILER_JAVA_HELPERS_H__
 #define GOOGLE_PROTOBUF_COMPILER_JAVA_HELPERS_H__
