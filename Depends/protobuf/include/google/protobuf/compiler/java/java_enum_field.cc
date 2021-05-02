2066ae74bbcefbcc30ec775c8afea82d0d7ba374
laser <laserhenry@gmail.com>
2021-05-02 13:19:29 +0100

    Update java_enum_field.cc

--- Depends/protobuf/include/google/protobuf/compiler/java/java_enum_field.cc
+++ Depends/protobuf/include/google/protobuf/compiler/java/java_enum_field.cc
@@ -31,6 +31,8 @@
 // Author: kenton@google.com (Kenton Varda)
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
+//  Startalk Ltd. 2021 revised
+
 
 #include <map>
 #include <string>
