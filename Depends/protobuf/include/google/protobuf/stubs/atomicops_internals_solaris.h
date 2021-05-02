02ef769013bb73dfd85c1dfbd713c0d5c5e7f0c0
laser <laserhenry@gmail.com>
2021-05-02 14:28:05 +0100

    Update atomicops_internals_solaris.h

--- Depends/protobuf/include/google/protobuf/stubs/atomicops_internals_solaris.h
+++ Depends/protobuf/include/google/protobuf/stubs/atomicops_internals_solaris.h
@@ -1,6 +1,7 @@
 // Copyright 2014 Google Inc. All rights reserved.
 // https://developers.google.com/protocol-buffers/
 //
+//  Startalk Ltd. 2021 revised
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
