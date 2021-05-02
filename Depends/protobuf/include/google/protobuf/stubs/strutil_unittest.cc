e9a2c088b00b86cea7eccbc663080db81a4e459f
laser <laserhenry@gmail.com>
2021-05-02 14:25:23 +0100

    Update strutil_unittest.cc

--- Depends/protobuf/include/google/protobuf/stubs/strutil_unittest.cc
+++ Depends/protobuf/include/google/protobuf/stubs/strutil_unittest.cc
@@ -29,6 +29,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Author: kenton@google.com (Kenton Varda)
+//  Startalk Ltd. 2021 revised
 
 #include <google/protobuf/stubs/strutil.h>
 
