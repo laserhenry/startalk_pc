6ac935a33f4e943e2aaa0660bfa97e40d72e0295
laser <laserhenry@gmail.com>
2021-05-02 14:26:17 +0100

    Update structurally_valid_unittest.cc

--- Depends/protobuf/include/google/protobuf/stubs/structurally_valid_unittest.cc
+++ Depends/protobuf/include/google/protobuf/stubs/structurally_valid_unittest.cc
@@ -1,5 +1,6 @@
 // Copyright 2008 Google Inc. All Rights Reserved.
 // Author: xpeng@google.com (Peter Peng)
+//  Startalk Ltd. 2021 revised
 
 #include <google/protobuf/stubs/common.h>
 #include <gtest/gtest.h>
