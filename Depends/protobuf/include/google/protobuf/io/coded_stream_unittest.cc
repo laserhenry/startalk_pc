6f06967c2ebd843f7e47f33eb1cbabb8ed731eb3
laser <laserhenry@gmail.com>
2021-05-02 14:13:48 +0100

    Update coded_stream_unittest.cc

--- Depends/protobuf/include/google/protobuf/io/coded_stream_unittest.cc
+++ Depends/protobuf/include/google/protobuf/io/coded_stream_unittest.cc
@@ -32,6 +32,7 @@
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
 //
+//  Startalk Ltd. 2021 revised
 // This file contains tests and benchmarks.
 
 #include <vector>
