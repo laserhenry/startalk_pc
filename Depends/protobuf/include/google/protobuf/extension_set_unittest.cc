6d3981ae55130b649129c7b606b7e459824a0a32
laser <laserhenry@gmail.com>
2021-05-02 13:53:45 +0100

    Update extension_set_unittest.cc

--- Depends/protobuf/include/google/protobuf/extension_set_unittest.cc
+++ Depends/protobuf/include/google/protobuf/extension_set_unittest.cc
@@ -31,6 +31,7 @@
 // Author: kenton@google.com (Kenton Varda)
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
+//  Startalk Ltd. 2021 revised
 
 #include <google/protobuf/extension_set.h>
 #include <google/protobuf/unittest.pb.h>
