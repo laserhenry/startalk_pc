dc396fea0ba3474aa6472f407d49dc388150e8da
laser <laserhenry@gmail.com>
2021-05-02 13:08:09 +0100

    Update main.cc

--- Depends/protobuf/include/google/protobuf/compiler/main.cc
+++ Depends/protobuf/include/google/protobuf/compiler/main.cc
@@ -29,6 +29,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Author: kenton@google.com (Kenton Varda)
+//  Startalk Ltd. 2021 revised
 
 #include <google/protobuf/compiler/command_line_interface.h>
 #include <google/protobuf/compiler/cpp/cpp_generator.h>
