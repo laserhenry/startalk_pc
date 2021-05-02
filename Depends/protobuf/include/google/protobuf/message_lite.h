e14bd7bdb7d1fd91d9ca341cf9a507199e5242ca
laser <laserhenry@gmail.com>
2021-05-02 14:07:23 +0100

    Update message_lite.h

--- Depends/protobuf/include/google/protobuf/message_lite.h
+++ Depends/protobuf/include/google/protobuf/message_lite.h
@@ -33,6 +33,7 @@
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
 //
+//  Startalk Ltd. 2021 revised
 // Defines MessageLite, the abstract interface implemented by all (lite
 // and non-lite) protocol message objects.
 
