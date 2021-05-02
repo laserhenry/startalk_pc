72a00f175fa002edc49831f0bf07577240c894cc
laser <laserhenry@gmail.com>
2021-05-02 14:08:08 +0100

    Update reflection_ops.cc

--- Depends/protobuf/include/google/protobuf/reflection_ops.cc
+++ Depends/protobuf/include/google/protobuf/reflection_ops.cc
@@ -31,6 +31,7 @@
 // Author: kenton@google.com (Kenton Varda)
 //  Based on original Protocol Buffers design by
 //  Sanjay Ghemawat, Jeff Dean, and others.
+//  Startalk Ltd. 2021 revised
 
 #include <string>
 #include <vector>
